800eb98e1d52377ffd56ea16d3fffcdc6e3f70fe
Eylem Seyhan <eylemseyhaann@gmail.com>
2024-10-08 14:57:31 +0300

    çakışmalar çözüldü

--- EventsProject/obj/Debug/net8.0/EventsProject.AssemblyInfo.cs
+++ EventsProject/obj/Debug/net8.0/EventsProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EventsProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88532758e1aa999d37bd4cfe3ce2ca0d4be0a0cf")]
 [assembly: System.Reflection.AssemblyProductAttribute("EventsProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EventsProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
