9b5bf4a09db3bded0b1c2616ef16b3b693e251af
ipek nur <114228895+ipeknroztrk@users.noreply.github.com>
2024-10-22 16:01:47 +0300

    Yapılan değişiklikler kaydedildi

--- EventsProject/obj/Debug/net8.0/EventsProject.AssemblyInfo.cs
+++ EventsProject/obj/Debug/net8.0/EventsProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EventsProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10517cb5f65a377ab5dda521fe8fc3209de4870f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c58964a70f959b867059d3aa1f6e45a154e13be")]
 [assembly: System.Reflection.AssemblyProductAttribute("EventsProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EventsProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
