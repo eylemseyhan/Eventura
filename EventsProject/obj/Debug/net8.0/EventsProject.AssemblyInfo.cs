d91c4e8f3cf9ec2d43d27dec10a4d879b89f0aed
esedagunduz <ebrargunduz07@gmail.com>
2024-10-22 19:12:01 +0300

    Local changes before merging Eylem branch

--- EventsProject/obj/Debug/net8.0/EventsProject.AssemblyInfo.cs
+++ EventsProject/obj/Debug/net8.0/EventsProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EventsProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11d110023145e631e0aa8bacdf9d1641975b1c1b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00d6af08c0f2be748ef8778df80c950383566e5a")]
 [assembly: System.Reflection.AssemblyProductAttribute("EventsProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EventsProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
