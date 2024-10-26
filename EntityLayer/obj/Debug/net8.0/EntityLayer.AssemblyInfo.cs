2bb495d16ba8b4d1b6bb79e2b4ed5ff4fdca2900
ipek nur <114228895+ipeknroztrk@users.noreply.github.com>
2024-10-26 15:23:25 +0300

    footer degişti

--- EntityLayer/obj/Debug/net8.0/EntityLayer.AssemblyInfo.cs
+++ EntityLayer/obj/Debug/net8.0/EntityLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EntityLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1284142db76c2769f475d6f711960c8727f1c94")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b5bf4a09db3bded0b1c2616ef16b3b693e251af")]
 [assembly: System.Reflection.AssemblyProductAttribute("EntityLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EntityLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- EventsProject/obj/Debug/net8.0/EventsProject.AssemblyInfo.cs
+++ EventsProject/obj/Debug/net8.0/EventsProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EventsProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c58964a70f959b867059d3aa1f6e45a154e13be")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b5bf4a09db3bded0b1c2616ef16b3b693e251af")]
 [assembly: System.Reflection.AssemblyProductAttribute("EventsProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EventsProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
