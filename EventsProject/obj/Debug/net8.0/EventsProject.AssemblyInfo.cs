65eb85a052765c2c3e993a44755521b2c0dabcfe
esedagunduz <ebrargunduz07@gmail.com>
2024-11-19 12:54:53 +0300

    Merge ipek branch into ebrar branch

--- EventsProject/obj/Debug/net8.0/EventsProject.AssemblyInfo.cs
+++ EventsProject/obj/Debug/net8.0/EventsProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EventsProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d91c4e8f3cf9ec2d43d27dec10a4d879b89f0aed")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84f959f934271dee1fc4269011917bdf2a7a67b0")]
 [assembly: System.Reflection.AssemblyProductAttribute("EventsProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EventsProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
