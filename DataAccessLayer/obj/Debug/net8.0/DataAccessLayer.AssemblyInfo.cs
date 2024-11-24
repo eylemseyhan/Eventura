7f2cc52251e5c54c8ffca60db0367223078176c1
ipek nur <114228895+ipeknroztrk@users.noreply.github.com>
2024-11-24 18:56:40 +0300

    sün güncelleme

--- DataAccessLayer/obj/Debug/net8.0/DataAccessLayer.AssemblyInfo.cs
+++ DataAccessLayer/obj/Debug/net8.0/DataAccessLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataAccessLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d58819a4017b3bb8c62fe61ec911d479bda67bd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36a4fa10c7af11d03f9b4188dcc29a392ec0a577")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataAccessLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataAccessLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
